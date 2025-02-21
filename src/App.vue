d05a9f9ea7eace6ee422bdda094124aec8373ce2
afei <917566188@qq.com>
2025-02-21 11:05:05 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <u-button>月落</u-button>
+</template>
+<script>
+import UAlertTips from "./uni_modules/vk-uview-ui/components/u-alert-tips/u-alert-tips.vue";
+
+export default {
+  components: {UAlertTips},
+  onLaunch: function () {
+    console.log('App Launch')
+  },
+  onShow: function () {
+    console.log('App Show')
+  },
+  onHide: function () {
+    console.log('App Hide')
+  },
+}
+</script>
+
+<style>
+@import "./uni_modules/vk-uview-ui/index.scss";
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import {
+	createSSRApp
+} from "vue";
+import App from "./App.vue";
+import uView from './uni_modules/vk-uview-ui';
+export function createApp() {
+	const app = createSSRApp(App);
+	app.use(uView)
+	return {
+		app,
+	};
+}
